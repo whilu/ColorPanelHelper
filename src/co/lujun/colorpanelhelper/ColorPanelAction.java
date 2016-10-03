6abbff1367558a238d701207881e0390b00289e1
whilu <lujun.byte@gmail.com>
2016-10-03 12:13:39 +0800

    Make action file

---
+++ src/co/lujun/colorpanelhelper/ColorPanelAction.java
@@ -0,0 +1,16 @@
+package co.lujun.colorpanelhelper;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Author: lujun(http://blog.lujun.co)
+ * Date: 2016/10/3 下午12:12
+ */
+public class ColorPanelAction extends AnAction {
+
+    @Override
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
