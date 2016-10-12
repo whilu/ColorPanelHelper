587e584c05c2e6be058d3024cbe3f0a6dfd26b59
whilu <lujun.byte@gmail.com>
2016-10-12 23:12:57 +0800

    Add: run with ui & logic

--- src/co/lujun/colorpanelhelper/ColorPanel.java
+++ src/co/lujun/colorpanelhelper/ColorPanel.java
@@ -1,8 +1,13 @@
 package co.lujun.colorpanelhelper;
 
 import com.intellij.openapi.editor.Editor;
+import org.apache.http.util.TextUtils;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -26,7 +31,7 @@ public class ColorPanel extends JFrame  {
     public ColorPanel(Editor editor, String hexColor){
         super("ColorPanelHelper");
         setContentPane(rootPanelContainer);
-        setPreferredSize(new Dimension(530, 260));
+        setPreferredSize(new Dimension(410, 160));
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
         pack();
         initView(editor, hexColor);
@@ -34,17 +39,35 @@ public class ColorPanel extends JFrame  {
     }
 
     private void initView(Editor editor, String hexColor){
-        hexColor = hexColor.replace("#", "");
-        int length = hexColor.length();
-        if (!isHexNumber(hexColor) || length < 6){
-            return;
-        }
-        String rgbColor = hexColor.substring(length - 6);
-        String hexAlpha = "FF";
-        if (length == 8){
-            hexAlpha = hexColor.substring(0, 2);
-        }
-        invalidateUI((int) convertHexStr2AlphaPercent(hexAlpha), hexAlpha, rgbColor);
+        sliderAlpha.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e) {
+                int alphaValue = sliderAlpha.getValue();
+                String hexColor = textFieldHexColor.getText();
+                if (hexColor.length() > 6){
+                    hexColor = convertAlphaValue2HexStr(alphaValue) + hexColor.substring(hexColor.length() - 6);
+                }else {
+                    hexColor = convertAlphaValue2HexStr(alphaValue) + hexColor;
+                }
+                invalidateUI(alphaValue, hexColor);
+            }
+        });
+        textFieldHexColor.getDocument().addDocumentListener(new DocumentListener() {
+            @Override
+            public void insertUpdate(DocumentEvent e) {
+                changeHexColorField();
+            }
+
+            @Override
+            public void removeUpdate(DocumentEvent e) {
+                changeHexColorField();
+            }
+
+            @Override
+            public void changedUpdate(DocumentEvent e) {
+                changeHexColorField();
+            }
+        });
         btnOk.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -52,15 +75,31 @@ public class ColorPanel extends JFrame  {
                 dispose();
             }
         });
+
+        hexColor = hexColor.replace("#", "");
+        int length = hexColor.length();
+        if (!isHexNumber(hexColor) || length < 6){
+            return;
+        }
+
+        if (length >= 8){
+            hexColor = hexColor.substring(length - 8);
+        }else if (hexColor.charAt(0) == '0'){
+            hexColor = "0" + hexColor;
+        }else {
+            hexColor = "F" + hexColor;
+        }
+        invalidateUI(convertHexStr2AlphaValue(hexColor.substring(0, 2)), hexColor);
     }
 
-    private String convertAlphaPercent2HexStr(float TPercent){
-        return Float.toHexString((1 - TPercent) * 255);
+    private String convertAlphaValue2HexStr(int value){
+        String result = Integer.toHexString((int) ((1 - (float) value / 100) * 255));
+        return result.equals("0") ? result + "0" : result;
     }
 
-    private float convertHexStr2AlphaPercent(String hexStr){
+    private int convertHexStr2AlphaValue(String hexStr){
         int value = Integer.parseInt(hexStr, 16);
-        return 1 - value / 255;
+        return (1 - value / 255) * 100;
     }
 
     private static boolean isHexNumber(String str){
@@ -74,14 +113,33 @@ public class ColorPanel extends JFrame  {
         return result;
     }
 
-    private void invalidateUI(int alphaPercent, String hexAlpha, String rgbColor){
-        sliderAlpha.setValue(alphaPercent * 100);
-        labelAlpha.setText(alphaPercent + "%");
-        textFieldHexColor.setText(hexAlpha + rgbColor);
-        textFieldA.setText(hexAlpha);
-        textFieldR.setText(rgbColor.substring(0, 2));
-        textFieldG.setText(rgbColor.substring(2, 4));
-        textFieldB.setText(rgbColor.substring(4, 6));
+    private void invalidateUI(int alphaValue, String hexColor){
+        if(TextUtils.isEmpty(hexColor) || hexColor.length() < 8 || !isHexNumber(hexColor)){
+            return;
+        }
+        sliderAlpha.setValue(alphaValue);
+        labelAlpha.setText(alphaValue + "%");
+        textFieldHexColor.setText(hexColor);
+        textFieldA.setText(hexColor.substring(0, 2));
+        textFieldR.setText(hexColor.substring(2, 4));
+        textFieldG.setText(hexColor.substring(4, 6));
+        textFieldB.setText(hexColor.substring(6, 8));
+    }
+
+    private void changeHexColorField(){
+        String hexColor = textFieldHexColor.getText();
+        int length = hexColor.length();
+        if (!isHexNumber(hexColor) || length < 6){
+            return;
+        }
+
+        String alphaHexStr;
+        if (length < 8){
+            alphaHexStr = hexColor.substring(0, length - 6);
+        }else {
+            alphaHexStr = hexColor.substring(length - 8, length - 6);
+        }
+        invalidateUI(convertHexStr2AlphaValue(alphaHexStr), hexColor.substring(length - 6));
     }
 
     private void injectHexColor(Editor editor, String color){
